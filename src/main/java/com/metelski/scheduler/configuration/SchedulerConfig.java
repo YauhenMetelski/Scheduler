12ab7d5dd3caf6f4a0ac909addce531f926491b3
Evgeny Metelsky <evgeny.metelsky@intexsoft.by>
2021-10-28 12:55:21 +0300

    Activate first scheduler

--- src/main/java/com/metelski/scheduler/configuration/SchedulerConfig.java
+++ src/main/java/com/metelski/scheduler/configuration/SchedulerConfig.java
@@ -1,19 +1,14 @@
 package com.metelski.scheduler.configuration;
 
-import com.metelski.scheduler.dao.CronDao;
 import com.metelski.scheduler.service.CronService;
-import org.quartz.Job;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.Trigger;
 import org.springframework.scheduling.TriggerContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
-import org.springframework.scheduling.config.TriggerTask;
 import org.springframework.scheduling.support.CronTrigger;
 
 import java.time.LocalTime;
@@ -24,8 +19,8 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
-//@Configuration
-//@EnableScheduling
+@Configuration
+@EnableScheduling
 public class SchedulerConfig implements SchedulingConfigurer {
     private List<String> cronExpressions;
     ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
